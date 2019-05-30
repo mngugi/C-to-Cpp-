22be37e909af881e0515f62352da095afe3010d3
mngugi <mngugi7@gmail.com>
2019-05-30 21:03:32 +0300

    Add files via upload

---
+++ HelloWorldFunc.c
@@ -0,0 +1,18 @@
+/****************************************************************
+* Author : Mwangi Ngugi
+* Purpose : a simple hello wprld using a function
+*
+*Function to me is the beging of coding , however learning basics
+* is important
+*****************************************************************/
+#include<stdio.h>
+void start()
+{
+	printf("Hello World C Coders");
+}
+int main()
+{
+   // calls the start function
+   start();
+   return 0;
+}
