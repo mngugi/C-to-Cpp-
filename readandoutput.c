989c7239882540a60b54071ccb745341666393df
mngugi <mngugi7@gmail.com>
2019-06-02 19:38:06 +0300

    Add files via upload

---
+++ readandoutput.c
@@ -0,0 +1,18 @@
+
+/****************************************************************
+* Author : Mwangi Ngugi
+* Purpose : Read and output characters
+*
+*
+*****************************************************************/
+#include<stdio.h>  // standard in put out put header function , which contains string package
+#include<conio.h>  // console character input function
+
+int main()
+{
+ char name[30];
+ printf("Enter your name:");
+ scanf("%s", &name);
+ printf("Welcome to C , %s", name);
+ getch();
+}
