e378cdb3d6673b1ca509cf66b6cd2ef83f2991de
M.ngugi <mngugi7@gmail.com>
2023-06-02 09:40:49 +0300

    Add files via upload

---
+++ InputProgam.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n ;
+	cout << "This is output.\n";
+	cout << "Enter Number: \n";
+	cin >> n ; 
+	
+	cout << n << "\nSquare Root: " << n*n << "\n";
+	return 0;
+}
