6639c77ed0ac28727c0e2863982588111fdc5f6d
M.ngugi <mngugi7@gmail.com>
2023-08-28 21:57:23 +0300

    Update footBallTeamClsTest.cpp

--- footBallTeamClsTest.cpp
+++ footBallTeamClsTest.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <string>
+#include <iomanip> // Include this for std::flush
 
-class footBallSquade {
+class footballSquad {
+
 public:
-    footBallSquade(const std::string& goalkeeper, const std::string& defender, const std::string& midfielder, const std::string& striker)
+    footballSquad(const std::string& goalkeeper, const std::string& defender, const std::string& midfielder, const std::string& striker)
         : goalkeeper(goalkeeper), defender(defender), midfielder(midfielder), striker(striker) {}
-/*formatPlayerInfo function that takes a player's name and a list of attributes.
-This function formats the output by concatenating the attributes with commas.
-It also removes the trailing comma and space at the end.*/
+
+    // Function to format player information
+    std::string formatPlayerInfo(const std::string& playerName, const std::initializer_list<std::string>& attributes) const {
+        std::string result = playerName + " - ";
+        for (const std::string& attribute : attributes) {
+            result += attribute + ", ";
+        }
+        result.erase(result.length() - 2); // Remove the trailing ", "
+        return result;
+    }
+
     std::string getGoalKeeper() const {
         return formatPlayerInfo(goalkeeper, {"Hands", "Feet", "Head", "Body"});
     }
@@ -25,15 +35,6 @@ It also removes the trailing comma and space at the end.*/
     }
 
 private:
-    std::string formatPlayerInfo(const std::string& playerName, const std::initializer_list<std::string>& attributes) const {
-        std::string result = playerName + " - ";
-        for (const std::string& attribute : attributes) {
-            result += attribute + ", ";
-        }
-        result.erase(result.length() - 2); // Remove the trailing ", "
-        return result;
-    }
-
     std::string goalkeeper;
     std::string defender;
     std::string midfielder;
@@ -41,12 +42,23 @@ private:
 };
 
 int main() {
-    footBallSquade keeperNumber1("Goalkeeper", "Defender", "Midfielder", "Striker");
+    std::string say = " Football Team Departments ";
+    std::string input;
 
-    std::cout << keeperNumber1.getGoalKeeper() << std::endl;
-    std::cout << keeperNumber1.getDefender() << std::endl;
-    std::cout << keeperNumber1.getMidfielder() << std::endl;
-    std::cout << keeperNumber1.getStriker() << std::endl;
+    // Create a footballSquad instance
+    footballSquad squad("Goalkeeper", "Defender", "Midfielder", "Striker");
+
+    std::cout << say << std::endl;
+    std::cout << "Goal keeper uses:" << squad.getGoalKeeper() << std::endl;
+    std::cout << squad.getDefender() << std::endl;
+    std::cout << squad.getMidfielder() << std::endl;
+    std::cout << squad.getStriker() << std::endl;
+
+    std:: cout << "Enter your Department:" << std::flush;
+    std:: cin >> input;
+    std:: cout << "You Entered: " << input << std::endl;
+
+
 
     return EXIT_SUCCESS;
 }
