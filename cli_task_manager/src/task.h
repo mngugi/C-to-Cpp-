2eead7c77eff21d1358463eecaceba6ebbd915a6
P.M.Ngugi <mngugi7@gmail.com>
2025-05-27 12:49:14 +0300

    Update task.h

---
+++ cli_task_manager/src/task.h
@@ -0,0 +1,21 @@
+// define the task structure
+
+// task.h
+#ifndef TASK_H
+#define TASK_H
+
+#define MAX_DESC_LEN 256
+
+typedef struct {
+    int id;
+    char description[MAX_DESC_LEN];
+    int done; // 0 = not done, 1 = done
+} Task;
+
+void add_task(const char *desc);
+void list_tasks();
+void delete_task(int id);
+void mark_task_done(int id);
+
+#endif
+
